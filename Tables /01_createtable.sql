f0433f3358add2f333991fa20e53c8b77d197725
Carl0sd4 <carlosdcampou@gmail.com>
2025-12-14 20:37:31 -0500

    update

--- Tables /01_createtable.sql
+++ Tables /01_createtable.sql
@@ -13,4 +13,57 @@ name varchar(10) NOT NULL,
 age int,
 email varchar(50),
 created date
+
+
+CREATE TABLE persons3 (
+id int NOT NULL,
+name varchar(10) NOT NULL,
+age int,
+email varchar(50),
+created datetime,
+UNIQUE(id)
+);
+
+CREATE TABLE persons4 (
+id int NOT NULL,
+name varchar(10) NOT NULL,
+age int,
+email varchar(50),
+created datetime,
+UNIQUE(id),
+PRIMARY KEY(id)
+);
+
+CREATE TABLE person5 (
+id int NOT NULL,
+name varchar(10) NOT NULL,
+age int,
+email varchar(50),
+created datetime,
+UNIQUE(id),
+PRIMARY KEY(id),
+CHECK(age>=18)
+);
+
+
+CREATE TABLE persons6 (
+id int NOT NULL,
+name varchar(100) NOT NULL,
+age int,
+email varchar(50),
+created datetime DEFAULT CURRENT_TIMESTAPM(),
+UNIQUE(id),
+PRIMARY KEY(id),
+CHECK(age>=18)
+);
+
+CREATE TABLE persons7 (
+id int NOT NULL AUTO_INCREMENT,
+name varchar(100) NOT NULL,
+age int,
+email varchar(50),
+created datetime DEFAULT CURRENT_TIMESTAMP(),
+UNIQUE(id),
+PRIMARY KEY(id),
+CHECK(age>=18)
 );
