c0d14d3a3d17bb9cfea3e2f245552e3fe9ec0c03
Carl0sd4 <carlosdcampou@gmail.com>
2025-12-18 20:58:27 -0500

    final del curso, antes de lecturas y comentarios del codigo

---
+++ 06_advanced/06_conectors.py
@@ -0,0 +1,55 @@
+import mysql.connector
+
+config = {
+    "host": "127.0.0.1",
+    "port": "3306",
+    "database": "hello_mysql",
+    "user": "root",
+    "password": "root1234"
+
+}
+
+connection = mysql.connector.connect(**config)
+cursor = connection.cursor()
+
+query = "SELECT * FROM users;"
+cursor.execute(query)
+result = cursor.fetchall()
+
+for row in result:
+    print(row)
+
+cursor.close()
+connection.close()
+
+
+
+
+import mysql.connector
+
+def print_user(user):
+
+    config = {
+        "host": "127.0.0.1",
+        "port": "3306",
+        "database": "hello_mysql",
+        "user": "root",
+        "password": "root1234"
+
+    }
+
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+
+    query = "SELECT * FROM users WHERE name=%s;"
+    print(query)
+    cursor.execute(query, (user,))
+    result = cursor.fetchall()
+
+    for row in result:
+        print(row)
+
+    cursor.close()
+    connection.close()
+
+print_user("Brais")
